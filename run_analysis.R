07a1df1d9b1fa20edccb6dcfcaef888861937af5
Isaac Moore <isaacymoore@gmail.com>
2016-03-14 20:28:29 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,37 @@
 # ----- Load activity labels and features data in R -----
-activitylabels <- read.table("activity_labels.txt")[,2]
-features <- read.table("features.txt")[,2]
+activitylabels <- read.table("activity_labels.txt")
+features <- read.table("features.txt")
+
 # ----- Load test data into R -----
 xTest <- read.table("X_test.txt")
 yTest <- read.table("y_test.txt")
 subjectTest <- read.table("subject_test.txt")
+
 # ----- Load train data into R -----
 xTrain <- read.table("X_train.txt")
 yTrain <- read.table("y_train.txt")
 subjectTrain <- read.table("subject_train.txt")
+
 # ----- Merge the train and test data -----
-x <- rbind(xTrain, xTest)
-y <- rbind(yTrain, yTest)
-subject <- rbind(subjectTrain, subjectTest)
+train <- cbind(xTrain, yTrain, subjectTrain)
+test <- cbind(xTest, yTest, subjectTest)
+
 # ----- Merge all data together -----
-allData <- cbind(x,y,subject)
+allData <- rbind(train, test)
+
 # ----- Filter mean and std from allData -----
-meanstdcols <- grepl("mean\\(\\)", names(allData)) | grepl("std\\(\\)", names(allData))
-meanstdcols[1:2] <- TRUE
-allData <- allData[, meanstdcols]
+meanstd <- grep(".*(mean|std).*", features$V2)
+
+# ----- Add titles -----
+titles <- c("Subject","activity")
+titles <- c(titles, as.character(features[meanstd,]$V2))
+names(allData) <- titles
+for (i in 1:6) {
+        allData$activity[allData$activity == i] <- as.character(activitylabels$V2[i])
+}
+
 # ----- Create the tidy dataset -----
 library(reshape2)
-melted <- melt(allData, id = c("subjectID", "activity"))
-tidyData <- dcast(melted, subjectID + activity ~ variable, mean)
+melted <- melt(allData, id = c("Subject", "activity"))
+tidyData <- dcast(melted, Subject + activity ~ variable, mean)
 write.table(tidyData, "tidyData.txt", row.name = FALSE) 
